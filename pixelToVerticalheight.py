2b5025a29dc8268ec5fae5883ebc1af8dc1a2362
DylCur <dylancurry001@gmail.com>
2023-05-13 14:42:07 +0100

    Initial Commit

---
+++ pixelToVerticalheight.py
@@ -0,0 +1,15 @@
+
+import os
+
+def main():
+
+    pixel = float(input("Enter the number of pixels: "))
+
+    print(f"{pixel / 10.8}")
+
+    input("Press enter to exit.")
+    os.system("cls")
+    main()
+    
+
+main()
