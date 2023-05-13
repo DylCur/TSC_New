1d869417b196a587b9350d18bfe38c17ef7af417
DylCur <dylancurry001@gmail.com>
2023-05-13 14:53:07 +0100

    Added a detailed description to the converter

--- pixelToVerticalheight.py
+++ pixelToVerticalheight.py
@@ -1,15 +1,30 @@
 
 import os
 
-def main():
+while True:
 
-    pixel = float(input("Enter the number of pixels: "))
+    pixel = input("Enter the number of pixels: ")
+    
+    if(pixel.isnumeric == False):
+        break
 
-    print(f"{pixel / 10.8}")
+    print(f"{float(pixel) / 10.8}")
 
     input("Press enter to exit.")
     os.system("cls")
-    main()
+    
+    
+    
+# In this script, you input an amount of pixels to be converted. The pixels
+# will be converted to css vh on a 1080p monitor. The maths involved is:
+# pixels / (monitor height / 100)
+
+# For example, if i enter 100 pixels to be converted:
+# 100 / ((1800 / 100) ≡ 10.8) = 9.25925925925926
+
+# The result will be rounded to the most appropriate amount of s.f.
+
+# In this case, the most appropriate answer would be 9.259 (4 s.f.)
+    
     
 
-main()
